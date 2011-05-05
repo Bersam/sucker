dd9742e2f0c0320748d203fc6f035fcf9fdd6509
aliva <ali.vakilzade@gmail.com>
2011-05-06 01:12:42 +0430

    HelloWorld Plugin for tests

---
+++ sucker/plugins/helloworld/HelloWorld.py
@@ -0,0 +1,9 @@
+class HelloWorld:
+    def __init__(self):
+        pass
+
+    def activate(self):
+        print ("Hello World Activated")
+
+    def deactivate(self):
+        print ("Hello World Deactivated")
