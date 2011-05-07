c3ad259f554132f7a73de3b162dd0ba352765309
Bersam <Bersam.k@gmail.com>
2011-05-07 15:59:47 +0430

    plugins changed to Plugins

--- sucker/__init__.py
+++ sucker/__init__.py
@@ -14,7 +14,7 @@ CONFIG_DIR = os.path.join(glib.get_user_config_dir(), 'sucker')
 if not os.path.isdir(CONFIG_DIR):
     os.makedirs(CONFIG_DIR)
 
-PLUGIN_DIRS = [os.path.join(ROOT_DIR, 'sucker/plugins')]
+PLUGIN_DIRS = [os.path.join(ROOT_DIR, 'sucker/Plugins')]
 
-if os.path.isdir(os.path.join(CONFIG_DIR, 'plugins')):
-    PLUGIN_DIRS.append(os.path.join(CONFIG_DIR, 'plugins'))
+if os.path.isdir(os.path.join(CONFIG_DIR, 'Plugins')):
+    PLUGIN_DIRS.append(os.path.join(CONFIG_DIR, 'Plugins'))
