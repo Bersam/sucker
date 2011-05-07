dace468c3df1e2854157e23db1a383d2b6fb671e
Bersam <Bersam.k@gmail.com>
2011-05-07 17:02:11 +0430

    aria2

---
+++ sucker/Plugins/aria2/aria2.py
@@ -0,0 +1,23 @@
+'''To use it first you must run aria2c --enable-rpc'''
+
+import xmlrpclib
+
+directory = "~/Download/aria2"
+server = "http://localhost"
+port ="6800"
+
+class aria2:
+    def __init__(self):
+        self.su = xmlrpclib.ServerProxy(server + ":" + port + "/rpc")
+    def addurl(self,url):
+    	self.su.aria2.addUri([url],dict(dir=directory))
+    	#return self.gid
+
+    def status(self,gid):
+    	su.aria2.tellStatus(gid)
+
+    def activate(self):
+        print ("aria2 plugin Activated")
+
+    def deactivate(self):
+        print ("aria2 plugin Deactivated")
