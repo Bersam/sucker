aa9c8ad6153db8619c920d6e8ba7bf739d1db18a
Bersam <Bersam.k@gmail.com>
2011-05-07 17:25:21 +0430

    aria2 added.

--- sucker/Plugins/aria2/aria2.py
+++ sucker/Plugins/aria2/aria2.py
@@ -9,12 +9,13 @@ port ="6800"
 class aria2:
     def __init__(self):
         self.su = xmlrpclib.ServerProxy(server + ":" + port + "/rpc")
+    
     def addurl(self,url):
     	self.su.aria2.addUri([url],dict(dir=directory))
     	#return self.gid
 
     def status(self,gid):
-    	su.aria2.tellStatus(gid)
+    	self.su.aria2.tellStatus(gid)
 
     def activate(self):
         print ("aria2 plugin Activated")
