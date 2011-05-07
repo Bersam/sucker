e6e0d9d0ef7cbcaced14ddb432edaa06e1f5d055
aliva <ali.vakilzade@gmail.com>
2011-05-07 17:59:10 +0430

    I prefer lower case for this directory! Don't change it again :D

--- sucker/__init__.py
+++ sucker/__init__.py
@@ -14,7 +14,7 @@ CONFIG_DIR = os.path.join(glib.get_user_config_dir(), 'sucker')
 if not os.path.isdir(CONFIG_DIR):
     os.makedirs(CONFIG_DIR)
 
-PLUGIN_DIRS = [os.path.join(ROOT_DIR, 'sucker/Plugins')]
+PLUGIN_DIRS = [os.path.join(ROOT_DIR, 'sucker/plugins')]
 
-if os.path.isdir(os.path.join(CONFIG_DIR, 'Plugins')):
-    PLUGIN_DIRS.append(os.path.join(CONFIG_DIR, 'Plugins'))
+if os.path.isdir(os.path.join(CONFIG_DIR, 'plugins')):
+    PLUGIN_DIRS.append(os.path.join(CONFIG_DIR, 'plugins'))
