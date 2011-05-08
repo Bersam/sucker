454834fef940cdd9e4815ce2b5a01877f9e12f1e
aliva <ali.vakilzade@gmail.com>
2011-05-08 22:03:26 +0430

    there is no need for this plugin

--- sucker/plugins/helloworld2/Hello.py
+++
@@ -1,9 +0,0 @@
-class Hello:
-    def __init__(self):
-        pass
-
-    def activate(self, shell):
-        print ("Hello world 2 activated")
-
-    def deactivate(self, shell):
-        print ("Hello world 2 deactivated")
