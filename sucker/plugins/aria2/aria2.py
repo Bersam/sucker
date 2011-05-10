3ded906d9dd052febf452f1f2a8a73d17d7f3752
Bersam <Bersam.k@gmail.com>
2011-05-10 12:22:37 -0700

    it need some multithreading in start_server function.

--- sucker/plugins/aria2/aria2.py
+++ sucker/plugins/aria2/aria2.py
@@ -1,27 +1,38 @@
 '''To use it first you must run aria2c --enable-rpc'''
 
-import xmlrpclib
+import xmlrpclib,pexpect
+import errno,socket
+import thread
 
 directory = "~/Download/aria2"
 server = "http://localhost"
 port ="6800"
 
 class aria2:
-    def __init__(self):
-        self.su = xmlrpclib.ServerProxy(server + ":" + port + "/rpc")
-    
-    def addurl(self,url):
-    	self.su.aria2.addUri([url],dict(dir=directory))
-    	#return self.gid
+	def __init__(self):
+		self.su = xmlrpclib.ServerProxy(server + ":" + port + "/rpc")
+	
+	def start_download(self,dic):
+		try:
+			url = dic['url']
+			self.su.aria2.addUri([url],dict(dir=directory))
+		except socket.error , e:
+			if e.errno == errno.ECONNREFUSED:
+				self.start_server()
+			else:
+				raise
 
-    def status(self,gid):
-    	self.su.aria2.tellStatus(gid)
+	def start_server(self):
+		ss = pexpect.run("aria2c --enable-rpc --rpc-listen-port %d" % (port))
+			 
+	def shutdown_server(self):
+		self.su.aria2.shutdown()
 
-    def activate(self, shell):
-        print ("aria2 plugin Activated")
+	def status(self,gid):
+		self.su.aria2.tellStatus(gid)
 
-    def deactivate(self, shell):
-        print ("aria2 plugin Deactivated")
+	def activate(self, shell):
+		print ("aria2 plugin Activated")
 
-    def start_download(self, dic):
-        print dic
+	def deactivate(self, shell):
+		print ("aria2 plugin Deactivated")
